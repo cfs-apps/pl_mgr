ce1a6273663739edb3d16216bc867f9f68da8f95
David McComas <open.stemware@gmail.com>
2026-02-23 13:49:43 -0500

    Preliminary version changes to test app store develop usage

--- fsw/src/app_cfg.h
+++ fsw/src/app_cfg.h
@@ -45,9 +45,10 @@
 ** Versions:
 **
 ** 1.0 - Initial release based on OpenSatKit Instrument Simulator(ISIM)
+** 2.0 - Compatible with Basecamp 4.0 and cFS 7.0
 */
 
-#define  PL_MGR_MAJOR_VER   1
+#define  PL_MGR_MAJOR_VER   2
 #define  PL_MGR_MINOR_VER   0
 
 
